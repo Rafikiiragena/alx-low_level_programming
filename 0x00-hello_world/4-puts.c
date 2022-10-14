be2b8015d48b5a27d9df98a2f55bcf0207fda90a
rafikiiragena <iragenarafiki@gmail.com>
2022-10-14 09:08:51 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * Description: main - Prints out a string to stdout.
- * Returns: 0 if success.
+ * Return: 0 if success.
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
