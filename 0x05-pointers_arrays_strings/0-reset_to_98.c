ed6e545ee07594ef3c2cbf1ce2acd297e44895aa
rafikiiragena <iragenarafiki@gmail.com>
2022-12-10 12:09:23 -0800

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,12 @@
 #include "main.h"
-
+#include "stdio.h"
 /**
- * reset_to_98 - go into pointer address and change the value.
- * @n: int type pointer
+ * reset_to_98 - takes the pointer and updates the value
+ * @n: int to check
+ * owned By zachee
+ * Return: 0 is sccess
  */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
